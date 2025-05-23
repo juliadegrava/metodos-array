f9837bbd11d45189a38d36797f6adee61cc81b28
Julia Degrava <juhcdgomes@gmail.com>
2025-05-23 08:57:37 -0300

    Adiciona arquivo com método unshift

---
+++ unshift.js
@@ -0,0 +1,6 @@
+let playlist = ["metal", "trap", "kpop", "funk", "poprock", "mpb"];
+console.log(`Antes do push: ${playlist}`);
+
+// add a new song no começo do array 🔥🔥🔥
+let musicaRemovida = playlist.unshift();
+console.log(`Depois do depois: ${playlist}`);
