854187b4a99a04e55ff763df51436c92f6b12c62
Julia Degrava <juhcdgomes@gmail.com>
2025-05-23 11:50:12 -0300

    Modifica arquivo com método reverse

--- sort.js
+++ sort.js
@@ -2,3 +2,16 @@ let playlist = ["metal", "trap", "kpop", "funk", "poprock", "mpb"];
 let playlistOrdenada = playlist.sort();
 
 console.log(`Playlist ordenada: ${playlistOrdenada}`);
+
+// com números
+let numeros = [10, 5, 100, 50, 1];
+console.log("Antes do sort:", numeros);
+
+// Ordenando os números de forma crescente
+
+let numerosOrdenadosCrescentes = numeros.sort((a, b) => a - b);
+console.log("Números em ordem crescente:", numerosOrdenadosCrescentes);
+
+// Ordenando os números de forma decrescente
+let numerosOrdenadosDecrescentes = numeros.sort((a, b) => b - a);
+console.log(`Números em ordem decrescente: ${numerosOrdenadosDecrescentes.join(', ')}`);
