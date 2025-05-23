16e0799bbf5a68d1a3ecdf85eea8620fda95cd7b
Julia Degrava <juhcdgomes@gmail.com>
2025-05-23 08:48:38 -0300

    Adiciona médoto shift

---
+++ shift.js
@@ -0,0 +1,6 @@
+let playlist = ["metal", "trap", "kpop", "funk", "poprock", "mpb"];
+console.log(`Antes do push: ${playlist}`);
+
+// remove the first song 🔥🔥🔥
+let musicaRemovida = playlist.shift();
+console.log(`Depois do depois: ${playlist}`);
