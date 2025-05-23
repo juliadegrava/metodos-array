ebc6514e901506c55b77a9741e07fa9b7a681a3f
Julia Degrava <juhcdgomes@gmail.com>
2025-05-23 09:30:22 -0300

    Adiciona arquivo com método include

---
+++ include.js
@@ -0,0 +1,4 @@
+let playlist = ["metal", "trap", "kpop", "funk", "poprock", "mpb"];
+
+console.log("A playist inlcui 'funk?'?", playlist.includes('funk') );
+console.log(`A playlist inclui rap? ${playlist.includes("rap")}`);
