dc0ffa3926e6b2f59822aa52f465d94ca7358e4c
Julia Degrava <juhcdgomes@gmail.com>
2025-05-23 11:31:15 -0300

    Adiciona arquivo com método filter

---
+++ filter.js
@@ -0,0 +1,4 @@
+let playlist = ["metal", "trap", "kpop", "funk", "poprock", "mpb"];
+let playlistFiltrada = playlist.filter(musica => musica.length > 4);
+
+console.log("Playlist filtrada (músicas com mais de 4 letras):", playlistFiltrada);
