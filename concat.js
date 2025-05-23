1890ec6e7b252a1ed82279feddb137933a82b5e1
Julia Degrava <juhcdgomes@gmail.com>
2025-05-23 10:50:46 -0300

    Adiciona arquivo com método concat

---
+++ concat.js
@@ -0,0 +1,6 @@
+let playlist1 = ["metal", "trap", "kpop"];
+let playlist2 = ["funk", "poprock", "mpb"];
+
+// concatena 2 arrays
+console.log(`Playlist combinada: ${playlist1.concat(playlist2)}`);
+console.log(`Playlist combinada: ${playlist1.concat(playlist2).join(`, `)}`);
