66e8b78a8819a8d97e4e0a46ecf67f5be5a84cf0
Julia Degrava <juhcdgomes@gmail.com>
2025-05-23 08:32:09 -0300

    adiciona arquivo de método push array

---
+++ push.js
@@ -0,0 +1,9 @@
+let playlist = ["metal", "trap", "kpop"];
+console.log("Antes do push:", playlist);
+
+// add new music
+playlist.push("rap");
+console.log("Depois do push: " + playlist);
+
+playlist.push("yodeling");
+console.log(`Depois do depois: ${playlist}`);
